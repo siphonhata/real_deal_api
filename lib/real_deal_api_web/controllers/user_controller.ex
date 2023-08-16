0b52f7c39eb75af5ff3639cebc1ade55680c361d
siphonhata <siphonhata@gmail.com>
2023-08-16 20:41:56 +0200

    Protecting endpoints

--- lib/real_deal_api_web/controllers/user_controller.ex
+++ lib/real_deal_api_web/controllers/user_controller.ex
@@ -15,7 +15,7 @@ defmodule RealDealApiWeb.UserController do
     with {:ok, %User{} = user} <- Users.create_user(user_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/users/#{user}")
+     # |> put_resp_header("location", ~p"/api/users/#{user}")
       |> render(:show, user: user)
     end
   end
