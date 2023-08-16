35c693088bbc457b15ec4cb3c6d7e50a9ad2ad21
siphonhata <siphonhata@gmail.com>
2023-08-16 15:08:06 +0200

    Working Bcrypt

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+
+config :real_deal_api, RealDealApiWeb.Auth.Guardian,
+    issuer: "real_deal_api",
+    secret_key: "i4sKnSQ/6wsvQgKLn+r08lIXAuFE3VRXLWQkyFXFIidV++S+Abv+8b1LnCfRNsVO"
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
---
+++ lib/real_deal_api/accounts/account.ex
@@ -0,0 +1,32 @@
+defmodule RealDealApi.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :hashed_password, :string
+    has_one :user, RealDealApi.Users.User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hashed_password])
+    |> validate_required([:email, :hashed_password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "Must have have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{hash_password: hashed_password}} = changeset) do
+
+      changes(changeset, hash_password: Bcrypt.hash_pwd_salt(hashed_password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
